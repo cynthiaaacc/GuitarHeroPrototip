88802ca32a0f03b98d5369b748183b36b0600638
Cintia Canela Canadell <57130702+cynthiaaacc@users.noreply.github.com>
2021-02-03 18:31:50 +0100

    prototipguitarhero

---
+++ New Unity Project/Assets/Scripts/ControlNotas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlNotas : MonoBehaviour
+{
+
+    private SpriteRenderer SpriteRenderer;
+    public Sprite nota;
+    public Sprite notaPresionada;
+    public KeyCode tecla;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(tecla))
+        {
+            SpriteRenderer.sprite = notaPresionada;
+        }
+
+        if(Input.GetKeyUp(tecla))
+        {
+            SpriteRenderer.sprite = nota;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Scroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    public float beat;
+
+    public bool iniciado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     beat = beat / 60f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!iniciado)
+        {
+            if(Input.anyKeyDown){
+                iniciado = true;
+            }
+        }else
+        {
+            transform.position -= new Vector3(0f, beat * Time.deltaTime, 0f);
+        }   
+    }
+}
